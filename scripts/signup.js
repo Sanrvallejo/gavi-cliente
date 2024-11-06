d864c4974cfc3a88c3abab324f13c3bf6550e6be
sanrvallejo <zanti-23@hotmail.com>
2024-11-06 08:48:43 -0500

    funcion: registro exitoso

--- scripts/signup.js
+++ scripts/signup.js
@@ -10,7 +10,7 @@ inputContrasena.addEventListener('input', () => {
   if (contrasena !== comprobacion) {
     inputContrasenaRespetida.style.borderColor = "red";
     inputContrasenaRespetida.style.borderWidth = "3px";
-  }else {
+  } else {
     inputContrasenaRespetida.style.borderColor = "#22aa46";
     inputContrasenaRespetida.style.borderWidth = "3px";
   }
@@ -24,11 +24,53 @@ inputContrasenaRespetida.addEventListener('input', () => {
   if (contrasena !== comprobacion) {
     inputContrasenaRespetida.style.borderColor = "red";
     inputContrasenaRespetida.style.borderWidth = "3px";
-  }else {
+  } else {
     inputContrasenaRespetida.style.borderColor = "#22aa46";
     inputContrasenaRespetida.style.borderWidth = "3px";
   }
 });
 
+//enviar formulario de registro
+const btnSend = document.querySelector('.send');
 
+btnSend.addEventListener('click', async function (event) {
+  event.preventDefault();
+
+  if (inputContrasena.value === inputContrasenaRespetida.value) {
+    const nombreIngresado = document.querySelector('#nombre').value;
+    const correoIngresado = document.querySelector('#correo').value;
+    const contrasenaIngresada = document.querySelector('#contrasena').value;
+
+    const usuario = {
+      nombre: nombreIngresado,
+      email: correoIngresado,
+      password: contrasenaIngresada
+    }
+
+    try {
+      let response = await fetch('http://localhost:8080/api/gavi/nuevo-usuario', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || `Error: ${response.status}`);
+      }
+  
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+
+    
+  } else {
+    alert("Compruebe los datos y que las constraseñas coincidan")
+  }
+  
+  window.location.href = "login.html";
+})
 
