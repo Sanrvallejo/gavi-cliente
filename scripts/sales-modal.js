1a0747f8d9a4f6e21956542b90699efad787da8e
sanrvallejo <zanti-23@hotmail.com>
2024-11-19 16:11:57 -0500

    funcion: mostrar modal de detalle de venta

---
+++ scripts/sales-modal.js
@@ -0,0 +1,31 @@
+const modalContent = document.querySelectorAll('.modal')[0];
+const modal = document.querySelectorAll('.content-modal')[0];
+const open = document.querySelectorAll(".btn-td")[0];
+const close = document.querySelectorAll('.close')[0];
+
+open.addEventListener('click', (e) => {
+  e.preventDefault()
+  modalContent.style.opacity = "1";
+  modalContent.style.visibility = "visible";
+  modal.classList.toggle('modal-close');
+});
+
+close.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.classList.toggle('modal-close');
+  setTimeout(() => {
+    modalContent.style.opacity = "0";
+    modalContent.style.visibility = "hidden";
+  }, 800);
+});
+
+modalContent.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (e.target == modalContent) {
+    modal.classList.toggle('modal-close');
+    setTimeout(() => {
+      modalContent.style.opacity = "0";
+      modalContent.style.visibility = "hidden";
+    }, 600);
+  }
+});
