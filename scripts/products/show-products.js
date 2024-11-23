8173a19d407276af47b324b6c51998cddcdb0840
sanrvallejo <zanti-23@hotmail.com>
2024-11-22 19:18:20 -0500

    funcion: editar productos

--- scripts/products/show-products.js
+++ scripts/products/show-products.js
@@ -1,10 +1,11 @@
-
+//evento
 document.addEventListener('DOMContentLoaded', async (e) => {
   e.preventDefault();
   const productsList = await getProducts();
   renderProducts(productsList);
 })
 
+//renderizar productos
 function renderProducts(productsList) {
   let table = document.querySelector('.product-table tbody');
   let productRow = '';
@@ -37,6 +38,7 @@ function renderProducts(productsList) {
   }
 }
 
+//hacer petición
 async function getProducts() {
   const usuario = JSON.parse(localStorage.getItem("loggedInUser"));
 
@@ -63,21 +65,22 @@ async function getProducts() {
   }
 }
 
+// evento buscar producto
 const btnSearch = document.querySelector('.btn-search');
 btnSearch.addEventListener('click', (e) => {
   e.preventDefault();
-  
+
   const productCode = document.querySelector('#product-code').value;
   const productName = document.querySelector('#product-name').value;
   const productsList = JSON.parse(localStorage.getItem('productsList'));
   const searchForm = document.querySelector('.form-search');
-  
+
   //buscar un producto por código
   if (productCode != "" && productCode != null) {
     const foundProduct = productsList.filter(product =>
       product.codigo.toLowerCase().includes(productCode)
     );
-    
+
     if (foundProduct != null || foundProduct.length < 0) {
       searchForm.reset();
       renderProducts(foundProduct);
@@ -85,13 +88,13 @@ btnSearch.addEventListener('click', (e) => {
       renderProducts([]);
     }
 
-  } 
-  
+  }
+
   if (productName != "" && productName != null) { //buscar producto por nombre
     const foundProduct = productsList.filter(product =>
       product.nombre.toLowerCase().includes(productName)
     );
-    
+
     if (foundProduct != null || foundProduct.length < 0) {
       searchForm.reset();
       renderProducts(foundProduct);
@@ -101,3 +104,29 @@ btnSearch.addEventListener('click', (e) => {
   }
 
 })
+
+//evento boton editar producto
+document.querySelector('.product-table').addEventListener('click', (e) => {
+  if (e.target.closest('.btn-td.edit-btn')) {
+    e.preventDefault();
+
+    const fila = e.target.closest("tr");
+    if (fila) {
+      const codigoProducto = fila.cells[0].textContent.trim();
+      const productsList = JSON.parse(localStorage.getItem('productsList')) || [];
+
+      const productoEncontrado = productsList.find(product =>
+        product.codigo.toLowerCase() === codigoProducto.toLowerCase()
+      );
+
+      if (productoEncontrado) {
+        localStorage.setItem('editProduct', JSON.stringify(productoEncontrado));
+        window.location.href = "edit-product.html";
+      } else {
+        alert("Producto no encontrado");
+      }
+    } else {
+      alert("Error al obtener la fila");
+    }
+  }
+});
